5a834d52cf513ff7292fe2a64302fa0492b0efe1
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-09-06 23:33:12 +0530

    Time: 8 ms (9.94%), Space: 23.4 MB (46.05%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,13 +1,15 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        for i in range(len(s)//2):
-            s[i] , s[ len(s) - i - 1] = s[len(s) - i - 1] , s[i]
-        return s
+        
 
         # two pointer approach
-        #left , right = 0 , len(s) - 1
-        #mid = ( left + right ) // 2
+        left , right = 0 , len(s) - 1
+        mid = ( left + right ) // 2
 
-        #while left <= mid :
-        #    s[left] , s[right] = s[right] , s[left]
-        #    left  , right  = left + 1 , right - 1
+        while left <= mid :
+           s[left] , s[right] = s[right] , s[left]
+           left  , right  = left + 1 , right - 1
+
+        #for i in range(len(s)//2): s[i] , s[ len(s) - i - 1] = s[len(s) - i - 1] , s[i]
+        
+        return s
