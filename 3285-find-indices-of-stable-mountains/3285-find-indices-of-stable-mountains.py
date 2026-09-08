30cebe41e91d24db6c5a15805eab158dba3fffaa
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-09-08 23:03:53 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.81%) - LeetHub

--- 3285-find-indices-of-stable-mountains/3285-find-indices-of-stable-mountains.py
+++ 3285-find-indices-of-stable-mountains/3285-find-indices-of-stable-mountains.py
@@ -1,7 +1,3 @@
 class Solution:
     def stableMountains(self, height: List[int], threshold: int) -> List[int]:
-        stable = []
-        for i in range(1,len(height)):
-            if height[i] != 0 and height[i - 1] > threshold :
-                stable.append(i)
-        return stable
+        return [ i for i in range(1,len(height)) if height[i-1] > threshold]
