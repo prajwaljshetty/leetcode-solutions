1c1614d5b0d16a1a77f39198bdca54f63e3bc194
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-09-10 00:44:22 +0530

    Time: 147 ms (30.62%), Space: 25.5 MB (90.96%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -3,11 +3,8 @@ class Solution:
         i , j = 0 , len(matrix[0]) - 1
 
         while i < len(matrix) and j >= 0:
-            if matrix[i][j] == target : 
-                return True
-            elif matrix[i][j] > target : 
-                j -= 1
-            else : 
-                i += 1
-
+            if matrix[i][j] == target : return True
+            elif matrix[i][j] > target : j -= 1
+            else : i += 1
+            
         return False
