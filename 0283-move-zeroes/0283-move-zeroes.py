863efc3c64a9b2e625fef60ccfe79fe0d534109f
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-09-12 20:52:07 +0530

    Time: 11 ms (16.01%), Space: 20.5 MB (25.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -8,7 +8,6 @@ class Solution:
             else :
                 if nums[right] != 0 :
                     nums[left],nums[right] = nums[right],nums[left]
-                    left += 1
-                    right += 1
+                    left , right = left + 1 , right + 1
                 else :
                     right += 1     
