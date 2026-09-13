131bd83fc7b4fbe2fb90c0e4e729afb0653d1f6e
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-09-14 01:48:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (10.53%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        bits = x ^ y
+        count = 0
+        while bits :
+            if bits & 1 : count += 1
+            bits >>= 1
+        return count 
