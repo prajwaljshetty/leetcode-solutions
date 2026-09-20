5e17e48251a35068523907ff0d2c3f05740b1b55
Prajwal Shetty <shettyprajwal009@gmail.com>
2026-09-20 16:44:33 +0530

    Time: 263 ms (86.55%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT id , movie , description , rating
 FROM Cinema
-WHERE id % 2 = 1 and description != 'boring'  
+WHERE id % 2 = 1 and description <> 'boring'  
 ORDER BY rating DESC;
